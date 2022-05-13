35a5ea44c2f178eb2b5a9cbd05f4d302daa256a4
TateArmstrong <90807397+TateArmstrong@users.noreply.github.com>
2022-05-13 17:54:15 -0500

    Added ball moving and collision checks

--- main.cpp
+++ main.cpp
@@ -5,16 +5,35 @@ int main(void){
     const int windowHeight = 720;
 
     InitWindow(windowWidth, windowHeight, "Crazy Game!");
-    SetTargetFPS(FLAG_VSYNC_HINT);
+    SetTargetFPS(144);
+
+    float xpos = GetScreenWidth()/2;
+    float ypos = GetScreenHeight()/2;
+    float ballRadius = 15;
+    float ballSpeedX = 300;
+    float ballSpeedY = 300;
 
     while(!WindowShouldClose()){
+
+        xpos += ballSpeedX * GetFrameTime();
+        ypos += ballSpeedY * GetFrameTime();
+
+        if (ypos + ballRadius > GetScreenHeight() || ypos - ballRadius < 0){
+            ballSpeedY *= -1;
+        }
+        if(xpos + ballRadius > GetScreenWidth() || xpos - ballRadius < 0){
+            ballSpeedX *= -1;
+        }
+
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
 
             DrawFPS(10, 10);
             
-            DrawCircle(xpos, ypos, 25, BLACK);
+            DrawCircle(xpos, ypos, ballRadius, BLACK);
+            DrawRectangle(10, GetScreenHeight()/2 - 100, 25, 200, BLACK);
+            DrawRectangle(GetScreenWidth() - 35, GetScreenHeight()/2 - 100, 25, 200, BLACK);
 
         EndDrawing();
     }
