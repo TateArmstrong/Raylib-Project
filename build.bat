ea05986ed1bc9af6dba08bc81308ae2082f90107
TateArmstrong <90807397+TateArmstrong@users.noreply.github.com>
2022-05-14 15:42:57 -0500

    Updated build.bat file

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-g++ *.cpp -o game.exe -O1 -Wall -std=c++17 -Wno-missing-braces -I include/ -L lib/ -lraylib -lopengl32 -lgdi32  -lwinmm
+g++ *.cpp -o game.exe -O1 -static -Wall -std=c++17 -Wno-missing-braces -I include/ -L lib/ -lraylib -lopengl32 -lgdi32  -lwinmm
 game
