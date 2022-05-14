82b642f7517da3dc8cc827175c0829f80760e9bd
TateArmstrong <90807397+TateArmstrong@users.noreply.github.com>
2022-05-14 00:03:48 -0500

    Refactored and added new structs

--- main.cpp
+++ main.cpp
@@ -1,28 +1,62 @@
 #include <raylib.h>
 
+struct Ball
+{
+    float x, y, speedX, speedY, radius;
+    
+    void Draw(){
+        DrawCircle(x, y, radius, BLACK);
+    }
+};
+
+struct Paddle
+{
+    float x, y, speed, width, height;
+
+    void Draw(Color color){
+        DrawRectangle(x - width/2, y - height/2,  width, height, color);
+    }
+};
+
 int main(void){
+
     const int windowWidth = 1280;
     const int windowHeight = 720;
 
     InitWindow(windowWidth, windowHeight, "Crazy Game!");
     SetTargetFPS(144);
 
-    float xpos = GetScreenWidth()/2;
-    float ypos = GetScreenHeight()/2;
-    float ballRadius = 15;
-    float ballSpeedX = 300;
-    float ballSpeedY = 300;
+    Ball ball;
+    ball.x = GetScreenWidth()/2;
+    ball.y = GetScreenHeight()/2;
+    ball.radius = 15;
+    ball.speedX = 300;
+    ball.speedY = 300;
+
+    Paddle leftPaddle;
+    leftPaddle.x = 30;
+    leftPaddle.y = GetScreenHeight()/2;
+    leftPaddle.speed = 300;
+    leftPaddle.width = 25;
+    leftPaddle.height = 200;
+
+    Paddle rightPaddle;
+    rightPaddle.x = GetScreenWidth() - 30;
+    rightPaddle.y = GetScreenHeight()/2;
+    rightPaddle.speed = 300;
+    rightPaddle.width = 25;
+    rightPaddle.height = 200;
 
     while(!WindowShouldClose()){
 
-        xpos += ballSpeedX * GetFrameTime();
-        ypos += ballSpeedY * GetFrameTime();
+        ball.x += ball.speedX * GetFrameTime();
+        ball.y += ball.speedY * GetFrameTime();
 
-        if (ypos + ballRadius > GetScreenHeight() || ypos - ballRadius < 0){
-            ballSpeedY *= -1;
+        if (ball.y + ball.radius > GetScreenHeight() || ball.y - ball.radius < 0){
+            ball.speedY *= -1;
         }
-        if(xpos + ballRadius > GetScreenWidth() || xpos - ballRadius < 0){
-            ballSpeedX *= -1;
+        if(ball.x + ball.radius > GetScreenWidth() || ball.x - ball.radius < 0){
+            ball.speedX *= -1;
         }
 
         BeginDrawing();
@@ -31,9 +65,9 @@ int main(void){
 
             DrawFPS(10, 10);
             
-            DrawCircle(xpos, ypos, ballRadius, BLACK);
-            DrawRectangle(10, GetScreenHeight()/2 - 100, 25, 200, BLACK);
-            DrawRectangle(GetScreenWidth() - 35, GetScreenHeight()/2 - 100, 25, 200, BLACK);
+            ball.Draw();
+            leftPaddle.Draw(BLACK);
+            rightPaddle.Draw(BLACK);
 
         EndDrawing();
     }
