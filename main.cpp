8726ca7970e40aa0f4f279e2d8a2b6afbc6baff7
TateArmstrong <90807397+TateArmstrong@users.noreply.github.com>
2022-05-16 23:22:45 -0500

    Revert "New raylib project"
    This reverts commit 4ece3cfd167e88bce9e137f6e23af4c28779a3ef.

--- main.cpp
+++ main.cpp
@@ -1,81 +1,145 @@
 #include <raylib.h>
-#include <rlgl.h>
-#include <iostream>
 
-struct Player{
-    float x, y, speed, radius, rotation;
-    Texture2D sprite;
+struct Ball
+{
+    float x, y, speedX, speedY, radius;
     
-    void initPlayer(float x_, float y_, float speed_, float radius_, float rotation, Texture sprite_){
-        x = x_;
-        y = y_;
-        speed = speed_;
-        radius = radius_;
-        sprite = sprite_;
-    }
-
-    Vector2 GetOrigin2(){
-        return {x, y};
-    }
-
-    Vector2 GetOrigin(){
-        return {x - 66.0f, y - 77.0f};
+    void Draw(){
+        DrawCircle(x, y, radius, BLACK);
     }
 };
 
-void PlayerInput(Player &player){
-    if(IsKeyDown(KEY_W)){
-        player.y -= player.speed * GetFrameTime();
+struct Paddle
+{
+    float x, y, speed, width, height;
+
+    // Returns a centered paddle(rectangle type)
+    Rectangle GetRect(){
+        return Rectangle {x - width/2, y - height/2, width, height};
     }
-    if(IsKeyDown(KEY_S)){
-        player.y += player.speed * GetFrameTime();
+
+    void Draw(Color color){
+        DrawRectangleRec(GetRect(), color);
     }
-    if(IsKeyDown(KEY_A)){
-        player.x -= player.speed * GetFrameTime();
-    }
-    if(IsKeyDown(KEY_D)){
-        player.x += player.speed * GetFrameTime();
-    }
-}
+};
 
 int main(void){
-    
+
     const int windowWidth = 1280;
     const int windowHeight = 720;
+    bool gameOver = false;
+    const char* winner = nullptr;
 
-    InitWindow(windowWidth, windowHeight, "Top Down Shooter Test");
+    InitWindow(windowWidth, windowHeight, "Crazy Game!");
     SetTargetFPS(144);
 
-    Player player; 
-    player.initPlayer((float)GetScreenWidth()/2, (float)GetScreenHeight()/2,
-         300.0f, 40.0f, 180.0f, LoadTexture("rsrc/top_down_man.png"));
+    Ball ball;
+    ball.x = GetScreenWidth()/2;
+    ball.y = GetScreenHeight()/2;
+    ball.radius = 15;
+    ball.speedX = 300;
+    ball.speedY = 300;
 
-    Rectangle soruceRect = {0.0f, 0.0f, player.sprite.width, player.sprite.height};
-    Rectangle destRect = {player.x, player.y, player.sprite.width, player.sprite.height};
+    Paddle leftPaddle;
+    leftPaddle.x = 30;
+    leftPaddle.y = GetScreenHeight()/2;
+    leftPaddle.speed = 600;
+    leftPaddle.width = 25;
+    leftPaddle.height = 200;
 
-    int rotation = 0;
+    Paddle rightPaddle;
+    rightPaddle.x = GetScreenWidth() - 30;
+    rightPaddle.y = GetScreenHeight()/2;
+    rightPaddle.speed = 600;
+    rightPaddle.width = 25;
+    rightPaddle.height = 200;
 
     while(!WindowShouldClose()){
 
-        // Player Inputs
-        PlayerInput(player);
+        ball.x += ball.speedX * GetFrameTime();
+        ball.y += ball.speedY * GetFrameTime();
 
-        //rotation++;
+        // Ball Wall Bouncing
+        if (ball.y + ball.radius > GetScreenHeight() || ball.y - ball.radius < 0){
+            ball.speedY *= -1;
+        }
+        // Bounces ball of the left and right walls
+        /*if(ball.x + ball.radius > GetScreenWidth() || ball.x - ball.radius < 0){
+            ball.speedX *= -1;
+        }*/
 
-        ClearBackground(RAYWHITE);
+        // Left Paddle Movement
+        if(IsKeyDown(KEY_W) && leftPaddle.y >= 100){
+            leftPaddle.y -= leftPaddle.speed * GetFrameTime();
+        }
+        if(IsKeyDown(KEY_S) && leftPaddle.y <= GetScreenHeight() - 100){
+            leftPaddle.y += leftPaddle.speed * GetFrameTime();
+        }
+
+        // Right Paddle Movement
+        if(IsKeyDown(KEY_UP) && rightPaddle.y > 100){
+            rightPaddle.y -= rightPaddle.speed * GetFrameTime();
+        }
+        if(IsKeyDown(KEY_DOWN) && rightPaddle.y < GetScreenHeight() - 100){
+            rightPaddle.y += rightPaddle.speed * GetFrameTime();
+        }
+
+        // Checks Collision for paddle and ball
+        if(CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, leftPaddle.GetRect())){
+            if(ball.speedX < 0){
+                ball.speedX *= -1.1f;
+                ball.speedY = (ball.y - leftPaddle.y) / (leftPaddle.height/2) * ball.speedX;
+            }
+        }
+        if(CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, rightPaddle.GetRect())){
+            if(ball.speedX > 0){
+                ball.speedX *= -1.1f;
+                ball.speedY = (ball.y - rightPaddle.y) / (rightPaddle.height/2) * -ball.speedX;
+            }
+        }
+
+        // Win conditions
+        if(ball.x > GetScreenWidth()){
+            gameOver = true;
+            winner = "Left Player Wins!";
+        }
+        if(ball.x < 0){
+            gameOver = true;
+            winner = "Right Player Wins!";
+        }
+
+        // Resets the game
+        if(gameOver && IsKeyPressed(KEY_SPACE)){
+            gameOver = false;
+            ball.y = GetScreenHeight()/2;
+            ball.x = GetScreenWidth()/2;
+            ball.speedX = 300;
+            ball.speedY = 300;
+            leftPaddle.x = 30;
+            leftPaddle.y = GetScreenHeight()/2;
+            rightPaddle.x = GetScreenWidth() - 30;
+            rightPaddle.y = GetScreenHeight()/2;
+        }
 
         BeginDrawing();
 
-            DrawRectangleRec(soruceRect, BLACK);
-            DrawRectangleRec(destRect, BLACK);
-            DrawTexturePro(player.sprite, soruceRect, destRect, player.GetOrigin2(), rotation, WHITE);
-            //DrawTextureEx(player.sprite, (Vector2){0.0f, 0.0f}, player.rotation, 300, WHITE);
-            DrawCircleV(player.GetOrigin2(), player.radius, BLACK);
+            ClearBackground(RAYWHITE);
+
+            DrawFPS(10, 10);
+            // Draws text when the game is over
+            if(gameOver){
+                int winnerTextWidth = MeasureText(winner, 50);
+                int resetTextWidth = MeasureText("Press space to restart", 30);
+                DrawText(winner, GetScreenWidth()/2 - winnerTextWidth/2, GetScreenHeight()/2 - 50, 50, BLACK);
+                DrawText("Press space to restart", GetScreenWidth()/2 - resetTextWidth/2, GetScreenHeight()/2 + 20, 30, BLACK);
+            }
+            
+            ball.Draw();
+            leftPaddle.Draw(BLACK);
+            rightPaddle.Draw(BLACK);
 
         EndDrawing();
     }
-
-    UnloadTexture(player.sprite);
 
     CloseWindow();
 
